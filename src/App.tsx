1f5494be0f38067deafe997df46fe1c78316a577
kugdev1011 <kug76501@gmail.com>
2024-12-11 05:01:16 -0800

    - fixed auth system and - added account list design

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from '@/pages/auth/Login'
-import Register from '@/pages/auth/Register'
-import ForgotPassword from '@/pages/auth/ForgotPassword'
-import Dashboard from '@/pages/dashboard/Dashboard.tsx'
-import PrivateRoute from '@/lib/PrivateRoute'
-import ResetPassword from '@/pages/auth/ResetPassword'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "@/pages/auth/Login";
+import Register from "@/pages/auth/Register";
+import ForgotPassword from "@/pages/auth/ForgotPassword";
+import Dashboard from "@/pages/dashboard/Dashboard.tsx";
+import PrivateRoute from "@/lib/PrivateRoute";
+import ResetPassword from "@/pages/auth/ResetPassword";
 import { Layout } from "@/app/Layout.tsx";
-import AdminList from "@/components/admin-management/AdminList.tsx";
+import AccountList from "@/components/admin-management/AccountList";
+import { AuthProvider } from "./lib/auth-util";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/" element={<PrivateRoute loginUrl={'/login'} />}>
-          <Route path="/dashboard" element={<Layout />}>
-            <Route index element={<Dashboard />}></Route>
-            <Route path="admin-list" element={<AdminList/>} />
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/" element={<PrivateRoute loginUrl={"/login"} />}>
+            <Route path="/dashboard" element={<Layout />}>
+              <Route index element={<Dashboard />}></Route>
+              <Route path="account-list" element={<AccountList />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
-    </Router>
-  )
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ui/datatable.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  PaginationState,
+  SortingState,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./select";
+import { Button } from "./button";
+import React from "react";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [pagination, setPagination] = React.useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+
+    //Pagination
+    getPaginationRowModel: getPaginationRowModel(),
+    onPaginationChange: setPagination,
+
+    // Sorting
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+
+    // Selection
+    onRowSelectionChange: setRowSelection,
+
+    state: {
+      rowSelection,
+      sorting,
+      pagination,
+    },
+  });
+
+  return (
+    <div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead
+                      key={header.id}
+                      className="text-center bg-sidebar font-black"
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4 gap-4">
+        <div className="flex items-center">
+          <span className="font-medium text-xs px-3 w-[11rem]">
+            Row per page
+          </span>
+          <Select
+            defaultValue={JSON.stringify(table.getState().pagination.pageSize)}
+            onValueChange={(e) => {
+              table.setPageSize(Number(e));
+            }}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="0" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="5">5</SelectItem>
+              <SelectItem value="10">10</SelectItem>
+              <SelectItem value="20">20</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <Button
+          variant="outline"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+}
