c4eda01dcb06a2d8442f77812bd13547b043efd4
kugdev1011 <kug76501@gmail.com>
2025-01-13 08:58:20 +0000

    Separate LivesessionList into two list: Upcoming & Started

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
 } from "react-router-dom";
 import Login from "@/pages/auth/Login";
 import Dashboard from "@/pages/dashboard/Dashboard.tsx";
@@ -13,72 +13,76 @@ import AccountList from "@/components/admin-management/AccountList";
 import { AuthProvider, useAuth } from "./lib/auth-util";
 import Profile from "./components/admin-management/Profile";
 import VideoLibrary from "./components/video-management/video-library";
-import LivestreamSessions from "@/components/livestream-management/LivestreamSessions.tsx";
+import LivestreamSessions
+	from "@/components/livestream-management/LivestreamSessions.tsx";
 import VideoStatistics from "./components/admin-management/VideoStatistic";
 import Register from "./pages/auth/Register";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import ResetPassword from "./pages/auth/ResetPassword";
 import LiveStatistic from "./components/admin-management/LiveStatistic";
 import {
-  APP_ACCOUNT_LIST_PATH,
-  APP_ACCOUNT_LOG_PATH,
-  APP_DASHBOARD_PATH,
-  APP_LIVE_CATEGORY_PATH,
-  APP_LIVE_LIST_PATH,
-  APP_LIVE_STATISTIC_PATH,
-  APP_LOGIN_PATH,
-  APP_PREFIX_PATH,
-  APP_PROFILE_PATH,
-  APP_VIDEO_LIBRARY_PATH,
-  APP_VIDEO_STATISTIC_PATH,
+	APP_ACCOUNT_LIST_PATH,
+	APP_ACCOUNT_LOG_PATH,
+	APP_DASHBOARD_PATH,
+	APP_LIVE_CATEGORY_PATH,
+	APP_LIVE_SESSION_PATH,
+	APP_LIVE_STATISTIC_PATH,
+	APP_LOGIN_PATH,
+	APP_PREFIX_PATH,
+	APP_PROFILE_PATH,
+	APP_VIDEO_LIBRARY_PATH,
+	APP_VIDEO_STATISTIC_PATH,
+	APP_UPCOMING_SESSION_PATH,
 } from "@/router";
 import AccountLog from "./components/admin-management/AccountLog";
 import LiveCategory from "./components/livestream-management/LiveCategory";
+import LiveSession from "@/pages/live-management/LiveSession.tsx";
+import UpcomingSession from "@/pages/live-management/UpcomingSession.tsx";
 
 function RedirectHome() {
-  const { isAuthenticated } = useAuth();
-  if (isAuthenticated) {
-    return <Navigate to={APP_DASHBOARD_PATH} replace />;
-  }
-
-  return <Navigate to={APP_LOGIN_PATH} replace />;
+	const { isAuthenticated } = useAuth();
+	if ( isAuthenticated ) {
+		return <Navigate to={APP_DASHBOARD_PATH} replace/>;
+	}
+	
+	return <Navigate to={APP_LOGIN_PATH} replace/>;
 }
+
 function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<RedirectHome />} />
-          <Route path={APP_LOGIN_PATH} element={<Login />} />
-          {/* <Route path="/register" element={<Register />} />
+	return (
+		<AuthProvider>
+			<Router>
+				<Routes>
+					<Route path="/" element={<RedirectHome/>}/>
+					<Route path={APP_LOGIN_PATH} element={<Login/>}/>
+					{/* <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} /> */}
-          <Route element={<PrivateRoute loginUrl={APP_LOGIN_PATH} />}>
-            <Route path={APP_DASHBOARD_PATH} element={<Layout />}>
-              <Route index element={<Dashboard />}></Route>
-              <Route path={APP_PROFILE_PATH} element={<Profile />} />
-              <Route path={APP_ACCOUNT_LIST_PATH} element={<AccountList />} />
-              <Route path={APP_ACCOUNT_LOG_PATH} element={<AccountLog />} />
-              <Route
-                path={APP_LIVE_LIST_PATH}
-                element={<LivestreamSessions />}
-              />
-              <Route path={APP_LIVE_CATEGORY_PATH} element={<LiveCategory />} />
-              <Route path={APP_VIDEO_LIBRARY_PATH} element={<VideoLibrary />} />
-              <Route
-                path={APP_LIVE_STATISTIC_PATH}
-                element={<LiveStatistic />}
-              />
-              <Route
-                path={APP_VIDEO_STATISTIC_PATH}
-                element={<VideoStatistics />}
-              />
-            </Route>
-          </Route>
-        </Routes>
-      </Router>
-    </AuthProvider>
-  );
+					<Route element={<PrivateRoute loginUrl={APP_LOGIN_PATH}/>}>
+						<Route path={APP_DASHBOARD_PATH} element={<Layout/>}>
+							<Route index element={<Dashboard/>}></Route>
+							<Route path={APP_PROFILE_PATH} element={<Profile/>}/>
+							<Route path={APP_ACCOUNT_LIST_PATH} element={<AccountList/>}/>
+							<Route path={APP_ACCOUNT_LOG_PATH} element={<AccountLog/>}/>
+							<Route path={APP_LIVE_SESSION_PATH} element={<LiveSession/>}/>
+							<Route path={APP_UPCOMING_SESSION_PATH}
+							       element={<UpcomingSession/>}/>
+							<Route path={APP_LIVE_CATEGORY_PATH} element={<LiveCategory/>}/>
+							<Route path={APP_VIDEO_LIBRARY_PATH} element={<VideoLibrary/>}/>
+							<Route
+								path={APP_LIVE_STATISTIC_PATH}
+								element={<LiveStatistic/>}
+							/>
+							<Route
+								path={APP_VIDEO_STATISTIC_PATH}
+								element={<VideoStatistics/>}
+							/>
+						</Route>
+					</Route>
+				</Routes>
+			</Router>
+		</AuthProvider>
+	);
 }
 
 export default App;
