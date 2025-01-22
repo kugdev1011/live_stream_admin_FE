418c1587bdd4c7335545af759a824bc4b48019a5
kugdev1011 <kug76501@gmail.com>
2025-01-22 20:42:27 +0800

    fixed video library page when build

--- src/services/auth-header.ts
+++ src/services/auth-header.ts
@@ -1,13 +1,13 @@
 export default function authHeader() {
-    const userStr = localStorage.getItem("user");
-    let user = null;
-    if (userStr) {
-			user = JSON.parse(userStr);
-    }
-
-    if (user && user.token) {
-      return { Authorization: `Bearer ${user.token}` }; // for Node.js Express back-end
-    } else {
-      return { Authorization: null }; // for Node Express back-end
-    }
+  const userStr = localStorage.getItem("user");
+  let user = null;
+  if (userStr) {
+    user = JSON.parse(userStr);
   }
+
+  if (user && user.token) {
+    return { Authorization: `Bearer ${user.token}` }; // for Node.js Express back-end
+  } else {
+    return { Authorization: "" }; // for Node Express back-end
+  }
+}
